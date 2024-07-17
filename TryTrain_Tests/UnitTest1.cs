eaffddeec3f3625fa68a6a8b8b2cca3a2a555593
Neralis <coopmaster333@gmail.com>
2024-07-17 18:52:02 +0300

    token fixed

--- TryTrain_Tests/UnitTest1.cs
+++ TryTrain_Tests/UnitTest1.cs
@@ -1,39 +1,55 @@
 using Microsoft.Playwright;
-using Microsoft.Win32;
-using System.Security.Principal;
-using System.Text;
+using System.Net;
+using System.Text.Json;
 
 namespace TryTrain_Tests
 {
-    public class AuthorizeTests
+    public class LoginTests
     {
         [SetUp]
-        public void Setup()
+        public async Task SetUp()
         {
-            
+
         }
 
         [Test]
         public async Task LoginTest()
         {
             var playwright = await Playwright.CreateAsync();
-            var requestContext = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
+
+            var request = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
             {
                 BaseURL = "https://localhost:7149/",
                 IgnoreHTTPSErrors = true
             });
 
-            var response = await requestContext.PostAsync(url:"Account/login", new APIRequestContextOptions()
+            var loginData = new Dictionary<string, object>  // Обратите внимание на тип значений object
             {
-                DataObject = new
-                {
-                   login = "Neralis",
-                   password = "9001"
-                }
+                { "login", "Neralis" },
+                { "password", "9001" }
+            };
+
+            var response = await request.PostAsync(url: "Account/login", new APIRequestContextOptions()
+            {
+                Params = loginData
             });
 
-            var jsonString = await response.JsonAsync();
+            var responseBody = await response.TextAsync();
 
+            // Десериализация JSON тела ответа в объект (предполагаем, что токен находится в поле 'token')
+            var responseObject = JsonSerializer.Deserialize<Dictionary<string, object>>(responseBody);
+            if (responseObject != null && responseObject.ContainsKey("token"))
+            {
+                var token = responseObject["token"].ToString();
+                // Используйте 'token' для дальнейших проверок или действий
+                Console.WriteLine($"Полученный токен: {token}");
+            }
+            else
+            {
+                Assert.Fail("Не удалось извлечь токен из ответа.");
+            }
+
+            //Assert.AreEqual(HttpStatusCode.OK, HttpStatusCode.OK);
         }
     }
 }
