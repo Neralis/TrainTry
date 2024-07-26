8b5d7423794b7568773120252430ceb55d360d79
Neralis <coopmaster333@gmail.com>
2024-07-26 16:30:20 +0300

    Юнит тесты DeleteMemorableDates закончены

--- TryTrain_Tests/UnitTests.cs
+++ TryTrain_Tests/UnitTests.cs
@@ -26,10 +26,10 @@ namespace UnitTests
             });
         }
 
-        #region [Удаление нулевого id]
-        
+        #region [Тест удаления нулевого id]
+
         [Test]
-        public async Task DeleteMemorableDateTest1()
+        public async Task DeleteMemorableDateTest_Zero()
         {
             var token = await GetToken();
 
@@ -45,6 +45,7 @@ namespace UnitTests
                 }
             });
 
+            Console.WriteLine(response.Status);
             Assert.AreEqual(404, response.Status);
         }
 
@@ -53,7 +54,7 @@ namespace UnitTests
         #region [Тест удаления id меньше нуля]
 
         [Test]
-        public async Task DeleteMemorableDateTest2()
+        public async Task DeleteMemorableDateTest_BelowZero()
         {
             var token = await GetToken();
 
@@ -69,15 +70,16 @@ namespace UnitTests
                 }
             });
 
+            Console.WriteLine(response.Status);
             Assert.AreEqual(404, response.Status);
         }
 
         #endregion
 
-        #region [Удаление id с слишком большим числом]
+        #region [Тест удаления id с максимальным значением Int32]
 
         [Test]
-        public async Task DeleteMemorableDateTest3()
+        public async Task DeleteMemorableDateTest_MaxInt32()
         {
             var token = await GetToken();
 
@@ -89,11 +91,106 @@ namespace UnitTests
                 },
                 DataObject = new Dictionary<string, object>
                 {
-                    { "id", "999999999999999999999999999999" }
+                    { "id", int.MaxValue.ToString() }
                 }
             });
 
+            Console.WriteLine(response.Status);
             Assert.AreEqual(404, response.Status);
+        }
+
+        #endregion
+
+        #region [Тест удаления id с вводом неверного типа данных ]
+
+        [Test]
+        public async Task DeleteMemorableDateTest_WrongValueType()
+        {
+            var token = await GetToken();
+
+            var response = await _request.DeleteAsync(url: "MemorableDates/DeleteMemorablesDates", new APIRequestContextOptions()
+            {
+                Headers = new Dictionary<string, string>
+                {
+                    { "Authorization", $"Bearer {token}" }
+                },
+                DataObject = new Dictionary<string, object>
+                {
+                    { "id", "s" }
+                }
+            });
+
+            Console.WriteLine(response.Status);
+            Assert.AreEqual(404, response.Status);
+        }
+
+        #endregion
+
+        #region [Тест удаления несуществующего id]
+
+        [Test]
+        public async Task DeleteMemorableDateTest_NonExistentId()
+        {
+            var token = await GetToken();
+
+            var response = await _request.DeleteAsync(url: "MemorableDates/DeleteMemorablesDates", new APIRequestContextOptions()
+            {
+                Headers = new Dictionary<string, string>
+                {
+                    { "Authorization", $"Bearer {token}" }
+                },
+                DataObject = new Dictionary<string, object>
+                {
+                    { "id", "123456" } // Assuming this ID doesn't exist
+                }
+            });
+
+            Console.WriteLine(response.Status);
+            Assert.AreEqual(404, response.Status);
+        }
+
+        #endregion
+
+        #region [Тест удаления id без авторизации]
+
+        [Test]
+        public async Task DeleteMemorableDateTest_Unauthorized()
+        {
+            var response = await _request.DeleteAsync(url: "MemorableDates/DeleteMemorablesDates", new APIRequestContextOptions()
+            {
+                DataObject = new Dictionary<string, object>
+                {
+                    { "id", "1" }
+                }
+            });
+
+            Console.WriteLine(response.Status);
+            Assert.AreEqual(401, response.Status);
+        }
+
+        #endregion
+
+        #region [Тест удаление с использованием неправильного HTTP метода]
+
+        [Test]
+        public async Task DeleteMemorableDateTest_WrongHttpMethod()
+        {
+            var token = await GetToken();
+
+            var response = await _request.PostAsync(url: "MemorableDates/DeleteMemorablesDates", new APIRequestContextOptions()
+            {
+                Headers = new Dictionary<string, string>
+                {
+                    { "Authorization", $"Bearer {token}" }
+                },
+                DataObject = new Dictionary<string, object>
+                {
+                    { "id", "7" }
+                }
+            });
+
+            Console.WriteLine(response.Status);
+            Assert.AreEqual(405, response.Status); // Assuming 405 Method Not Allowed
         }
 
         #endregion
